326100e5bb335b682106cdfe9bd8a0c14ce421b9
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-26 15:27:02 +0900

    라우터 적용

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import Navbar from "./layout/Navbar";
 import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddUser from "./users/AddUser";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/adduser" element={<AddUser />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/users/AddUser.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddUser = () => {
+  return <div>유저추가</div>;
+};
+
+export default AddUser;
