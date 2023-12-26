5afd7e69b1383ef855e0a08a00c0d2da0be683a1
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-26 15:04:23 +0900

    axios 설치 와 백엔드 서버에 모든유저 가져오기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // 시작시 유저들 가져오기
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const result = await axios.get("http://localhost:8080/users");
+    console.log(result);
+  };
+
   return (
     <div className="container">
-      <table className="table border shadow my-4"></table>
+      <table className="table border shadow my-4">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
