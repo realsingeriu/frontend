45138cc1920803d9b390adf891bff5e98adcf840
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-26 14:34:20 +0900

    네브바 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import Navbar from "./layout/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Navbar() {
-  return <div>네브바</div>;
+  return (
+    <nav className="navbar navbar-expand-md bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand text-warning" href="#">
+          리액트 & 스프링부트
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <button className="btn btn-outline-light">유저 추가</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
